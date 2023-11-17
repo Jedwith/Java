8e43b922aa7ea606e15cbb064bce9ea79004f7b5
Jedwith <Ega346@gmail.com>
2023-11-17 11:27:36 +0300

    Task 4 (5 вариант)

---
+++ Task 4.5/lr4/src/main/java/org/example/Apple.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Apple extends Fruit implements AvoidFruit<Apple>{
+
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ Task 4.5/lr4/src/main/java/org/example/Orange.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Orange extends Fruit implements AvoidFruit<Orange>{
+    public Orange() {
+        super(1.5f);
+    }
+}
