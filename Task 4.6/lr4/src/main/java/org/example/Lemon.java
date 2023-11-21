697b38d640d7dae5e25b837e78e6c06371db746f
Jedwith <Ega346@gmail.com>
2023-11-21 12:46:10 +0300

    Task 6

--- Task 4.6/lr4/src/main/java/org/example/Lemon.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Lemon extends Fruit {
-    public Lemon() {
-        super(2f);
-    }
-}
--- Task 4.6/lr4/src/main/java/org/example/Orange.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Orange extends Fruit {
-    public Orange() {
-        super(1.5f);
-    }
-}
